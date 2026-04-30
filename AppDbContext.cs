d64227ad3e4c7671c5b6e70056c854dc0f096e87
Ekta Nema <ektanema123@gmail.com>
2026-04-30 12:10:24 +0530

    Add project files.

---
+++ AppDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+
+namespace FirstCoreWebApp
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+        public DbSet<User> Users { get; set; }  
+    }
+}
---
+++ Controllers/AuthController.cs
@@ -0,0 +1,107 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+using System.Text.RegularExpressions;
+
+namespace FirstCoreWebApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public AuthController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpPost("registers")]
+        public async Task<IActionResult> Registers(Register reg)
+        {
+            //Email Validatio
+            if(!Regex.IsMatch(reg.Email, @"^[^@\s]+@[^@\s]+\.[^@\s]+$"))
+            {
+                return BadRequest("Invalid Email Format");
+            }
+
+            //Duplicate Email
+            if(_context.Users.Any(u=>u.Email == reg.Email))
+            {
+                return StatusCode(409, "Email Already Exists in the system");
+            }
+
+            if(!(IsValidaPassword(reg.Password)))
+            {
+                return BadRequest("Password must be at least 8 characters and include uppercase, lowercase, number, and special character");
+            }
+
+            string hashPswd = BCrypt.Net.BCrypt.HashPassword(reg.Password);
+
+            var user = new User
+            {
+                Name = reg.Name,
+                Email = reg.Email,
+                PasswordHash = hashPswd
+            };
+            _context.Users.Add(user);
+            await _context.SaveChangesAsync();
+
+            return StatusCode(201, new
+            {
+                message = "User Registered Successfully",
+                userId = user.Id
+            });
+
+        }
+
+        private bool IsValidaPassword(string password)
+        {
+            if(password.Length < 8)
+            {
+                return false;
+            }
+            if(!Regex.IsMatch(password, @"[A-Z]"))
+            {
+                return false;
+            }
+            if (!Regex.IsMatch(password, @"[a-z]"))
+            {
+                return false;
+            }
+            if (!Regex.IsMatch(password, @"[0-9]"))
+            {
+                return false;
+            }
+            if (!Regex.IsMatch(password, @"[\W_]"))
+                return false;
+
+            return true;
+        }
+
+        [HttpPost("loging")]
+        public async Task<IActionResult> Logins(Login log)
+        {
+            if (!Regex.IsMatch(log.Email, @"^[^@\s]+@[^@\s]+\.[^@\s]+$"))
+            {
+                return BadRequest("Invalid Email Format");
+            }
+            var user = _context.Users.FirstOrDefault(u=>u.Email == log.Email);
+            if (user == null)
+            {
+                return StatusCode(401, "Invalid Credential");
+            }
+            bool isvalid = BCrypt.Net.BCrypt.Verify(log.Password,user.PasswordHash );
+            if(!isvalid) 
+            {
+                return StatusCode(401, "Invalid Credential");
+            }
+            return StatusCode(201,new
+            {
+                message = "Login Successfully",
+                userid = user.Id,
+                email = user.Email,
+            });
+        }
+    }
+}
